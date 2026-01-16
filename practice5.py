c088d6acf0a41adea9ac5a466918a67fb9c51c39
Dishant Bundheliya <dishantbundheliya2712@gmail.com>
2026-01-16 19:55:57 +0530

    attendance eligibility check for exams

---
+++ practice5.py
@@ -0,0 +1,6 @@
+attendence = float(input("Enter your attendence Percentage:"))
+
+if attendence>=75:
+    print("Cograts! you are eligible for the exam")
+else:
+    print("Oops! you are not eligible for the exam")
