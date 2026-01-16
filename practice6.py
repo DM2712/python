c6137f21ebb29baa895dc77e70e12302288cce02
Dishant Bundheliya <dishantbundheliya2712@gmail.com>
2026-01-16 19:56:32 +0530

    age-based travel charge calculation

---
+++ practice6.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age:"))
+
+if age<=5 :
+    print("You can travel without paying")
+elif age>5 and age<=12 :
+    print("Your charge for the Travel is 50/- RS")
+elif age>12 and age<=60 :
+    print("Your charge for the Travel is 100/- RS")
+else :
+    print("Your charge for the Travel is 40/- RS")
+    
